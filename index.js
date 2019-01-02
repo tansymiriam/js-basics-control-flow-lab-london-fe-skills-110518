834bfeaea9f326b41cae45a18cf47c7a959f2ee6
Tansy Roberts <tansymiriam@gmail.com>
2019-01-02 10:47:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let result
+  
+  if (someValue < 400) {
+  result = 'This one is on me!';
+  } 
+  
+  else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.'; 
+    }
+    else if (someValue > 2500) {
+      result = 'No can do.';
+    }
+return result
+}
+
+function ternaryCheckCity()
